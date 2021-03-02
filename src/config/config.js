8b063884171bc05965277b591c6d3e4830475ff0
Waqas Ajaz <waqasajazch@gmail.com>
2021-03-02 18:10:46 +0500

    Added environment files

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,7 @@
 const config = {
-  apiUrl: 'http://flashboard.socialmarketanalytics.com',
-  comparisionSite: 'http://engine-spirit.s-factors.com/dictionary/compare',
-  agGridLicense:
-    'For_Trialing_ag-Grid_Only-Not_For_Real_Development_Or_Production_Projects-Valid_Until-18_March_2021_[v2]_MTYxNjAyNTYwMDAwMA==948d8f51e73a17b9d78e03e12b9bf934'
+  apiUrl: process.env.REACT_APP_API_URL,
+  comparisionSite: process.env.REACT_APP_COMPARISION_SITE_URL,
+  agGridLicense: process.env.REACT_APP_AGGRID_LICENSE
 };
 
 export default config;
