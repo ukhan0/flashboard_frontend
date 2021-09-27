0929d8afee6f992b8c3e5947b5fb9fdedc9b499f
rehan510 <rehan51055@gmail.com>
2021-09-27 16:24:02 +0500

    resolved warning issue

--- src/components/sentiment/Sentiment.js
+++ src/components/sentiment/Sentiment.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector, useDispatch } from 'react-redux';
@@ -23,26 +23,30 @@ const Sentiment = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
   const history = useHistory();
+  const firstTimeLoad = useRef(false);
   let getQueryParams = new URLSearchParams(useLocation().search);
   if (!getQueryParams.get('recentId') && !selectedItem) {
     history.push('/watchlist');
   }
   useEffect(() => {
-    if (
-      getQueryParams.get('recentId') &&
-      getQueryParams.get('ticker') &&
-      getQueryParams.get('industry') &&
-      getQueryParams.get('sector')
-    ) {
-      const sentimentData = {};
-      sentimentData.industry = getQueryParams.get('industry');
-      sentimentData.sector = getQueryParams.get('sector');
-      sentimentData.ticker = getQueryParams.get('ticker');
-      sentimentData.recentId = getQueryParams.get('recentId');
-      dispatch(setSelectedWatchlist(sentimentData));
+    if (!firstTimeLoad.current) {
+      firstTimeLoad.current = true;
+      if (
+        getQueryParams.get('recentId') &&
+        getQueryParams.get('ticker') &&
+        getQueryParams.get('industry') &&
+        getQueryParams.get('sector')
+      ) {
+        const sentimentData = {};
+        sentimentData.industry = getQueryParams.get('industry');
+        sentimentData.sector = getQueryParams.get('sector');
+        sentimentData.ticker = getQueryParams.get('ticker');
+        sentimentData.recentId = getQueryParams.get('recentId');
+        dispatch(setSelectedWatchlist(sentimentData));
+      }
+      dispatch(getSentimentData());
     }
-    dispatch(getSentimentData());
-  }, [dispatch]);
+  }, [dispatch, getQueryParams]);
 
   const handleSelection = path => {
     setTimeout(() => {
