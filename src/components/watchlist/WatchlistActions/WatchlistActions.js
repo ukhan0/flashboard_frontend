54f628d80efd618c9da87a7627352d30532b8868
Saqlain Ali Javed <67510524+saqi3492@users.noreply.github.com>
2022-08-24 14:02:16 +0500

    add file type email alert in grid table actions (#396)
    * add file type email alert in grid table actions
    
    * remove warning
    
    * add button
    
    * change styling

--- src/components/watchlist/WatchlistActions/WatchlistActions.js
+++ src/components/watchlist/WatchlistActions/WatchlistActions.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { ListItem, List, ListItemText, Switch, Grid, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { ListItem, List, ListItemText, Switch, Grid, Typography, Menu, MenuItem, Button } from '@material-ui/core';
 import WatchlistService from '../WatchlistService';
 import { useDispatch, useSelector } from 'react-redux';
 import { setIsColorEnable, setIsActiveCompanies } from '../../../reducers/Watchlist';
 import { updateWatchlistEmailAlertStatus } from './WatchlistActionApiCalls';
 import { getUser, saveUser } from '../WatchlistHelpers';
+import FileTypes from '../../../config/watchlistFileTyes';
+import config from '../../../config/config';
+import { setWatchlistFileTypesEmailAlertStatus } from './../../../reducers/Watchlist';
+import { cloneDeep, get } from 'lodash';
+import axios from 'axios';
 
 export default function WatchListActions() {
   const dispatch = useDispatch();
-  const { isColorEnable, isActiveCompanies } = useSelector(state => state.Watchlist);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const { isColorEnable, isActiveCompanies, fileTypesEmailAlertStatus } = useSelector(state => state.Watchlist);
   const actions = [
     { key: 'autoSize', label: 'Auto Size Columns' },
     // { key: 'sizeToFit', label: 'Fit Column Size' },
@@ -53,48 +60,172 @@ export default function WatchListActions() {
     user.enable_watchlist_color = status;
     saveUser(user);
   };
-  return (
-    <List component="nav">
-      {actions.map((action, index) => {
-        return (
-          <ListItem
-            key={index}
-            button
-            onClick={() => {
-              handleClick(action.key);
-            }}>
-            <ListItemText primary={action.label} />
-          </ListItem>
+
+  const onEmailStatusChange = async (e, send_email, doc_type) => {
+    try {
+      e.stopPropagation();
+      let fileTypesInfoObj = cloneDeep(fileTypesEmailAlertStatus);
+      const fileIndex = fileTypesInfoObj.findIndex(fileTypeInfoObj => fileTypeInfoObj.filetype === doc_type);
+      if (fileIndex !== -1) {
+        fileTypesInfoObj[fileIndex].isEmailEnable = send_email;
+      } else {
+        fileTypesInfoObj.push({
+          filetype: doc_type,
+          isEmailEnable: send_email
+        });
+      }
+      dispatch(setWatchlistFileTypesEmailAlertStatus(fileTypesInfoObj));
+
+      const response = await axios.post(`${config.apiUrl}/api/update_doc_email_status`, {
+        doc_type,
+        send_email
+      });
+      const isError = get(response, 'data.error', true);
+      if (isError) {
+        let previousFileTypesInfoObj = cloneDeep(fileTypesEmailAlertStatus);
+        const addedObjectIndex = previousFileTypesInfoObj.findIndex(
+          fileTypeInfoObj => fileTypeInfoObj.filetype === doc_type
         );
-      })}
-      <Grid container direction="row" alignItems="center">
-        <Grid item>
-          <Switch
-            checked={isColorEnable}
-            onChange={handleChange}
-            color="primary"
-            name="checkedB"
-            inputProps={{ 'aria-label': 'primary checkbox' }}
-          />
+        previousFileTypesInfoObj[addedObjectIndex].isEmailEnable = !send_email;
+        dispatch(setWatchlistFileTypesEmailAlertStatus(previousFileTypesInfoObj));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getEmailStatus = fileType => {
+    const filteredFileType = fileTypesEmailAlertStatus.find(type => type.filetype === fileType);
+    return filteredFileType?.isEmailEnable ? true : false;
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <List component="nav">
+        {actions.map((action, index) => {
+          return (
+            <ListItem
+              key={index}
+              button
+              onClick={() => {
+                handleClick(action.key);
+              }}>
+              <ListItemText primary={action.label} />
+            </ListItem>
+          );
+        })}
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Switch
+              checked={isColorEnable}
+              onChange={handleChange}
+              color="primary"
+              name="checkedB"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography>Enable Colors</Typography>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Typography>Enable Colors</Typography>
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <Switch
+              checked={isActiveCompanies}
+              onChange={handleActive}
+              color="primary"
+              name="checkedB"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography>Show Active Companies</Typography>
+          </Grid>
         </Grid>
+      </List>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={e => setAnchorEl(e.currentTarget)}
+        variant={'outlined'}
+        color={'primary'}
+        style={{ margin: '5px 0 0 10px' }}
+      >
+        File Type Email Alerts
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+        style={{ height: '80%' }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}>
+        {FileTypes.map((file, index) => {
+          const isEnable = getEmailStatus(file.label);
+          return (
+            <MenuItem key={index}>
+              <Grid
+                style={{ width: '160px' }}
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+              >
+                <Grid item>
+                  <Typography>{file.label}</Typography>
+                </Grid>
+                <Grid item>
+                  <Switch
+                    checked={isEnable}
+                    onClick={e => onEmailStatusChange(e, !isEnable, file.label)}
+                    color="primary"
+                    name="checkedB"
+                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                  />
+                </Grid>
+              </Grid>
+            </MenuItem>
+          );
+        })}
+      </Menu>
+      {/* <Grid item>
+        <Typography style={{ marginTop: '15px', paddingLeft: '10px' }}>Email Alerts!</Typography>
       </Grid>
-      <Grid container direction="row" alignItems="center">
-        <Grid item>
-          <Switch
-            checked={isActiveCompanies}
-            onChange={handleActive}
-            color="primary"
-            name="checkedB"
-            inputProps={{ 'aria-label': 'primary checkbox' }}
-          />
-        </Grid>
-        <Grid item>
-          <Typography>Show Active Companies</Typography>
-        </Grid>
-      </Grid>
-    </List>
+      {FileTypes.map((file, index) => {
+        const isEnable = getEmailStatus(file.label);
+        return (
+          <Grid container direction="row" alignItems="center" key={index}>
+            <Grid item>
+              <Switch
+                checked={isEnable}
+                onClick={e => onEmailStatusChange(e, !isEnable, file.label)}
+                color="primary"
+                name="checkedB"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography>{file.label}</Typography>
+            </Grid>
+          </Grid>
+        );
+      })} */}
+    </>
   );
 }
