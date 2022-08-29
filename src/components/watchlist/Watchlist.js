0a1502da152304d00d01ebe9e8722dc417d9f464
Muhammad Rehan <73136316+Rehan510@users.noreply.github.com>
2022-08-29 05:09:09 -0400

    resolved warnings (#408)

--- src/components/watchlist/Watchlist.js
+++ src/components/watchlist/Watchlist.js
@@ -113,19 +113,19 @@ const Watchlist = props => {
   const [syncData, setSyncData] = useState([]);
   const [gridData, setGridData] = useState(null);
   const [gridData2, setGridData2] = useState([]);
-  // let completeCompaniesDatalocal = useRef(completeCompaniesData);
-  // let completeCompaniesDataGloballocal = useRef(completeCompaniesDataGlobal);
+  let completeCompaniesDatalocal = useRef(completeCompaniesData);
+  let completeCompaniesDataGloballocal = useRef(completeCompaniesDataGlobal);
   const anchorOrigin = { vertical: 'bottom', horizontal: 'left' };
 
-  // useEffect(() => {
-  //   completeCompaniesDatalocal.current = completeCompaniesData;
-  //   completeCompaniesDataGloballocal.current = completeCompaniesDataGlobal;
-  // }, [completeCompaniesData, completeCompaniesDataGlobal]);
+  useEffect(() => {
+    completeCompaniesDatalocal.current = completeCompaniesData;
+    completeCompaniesDataGloballocal.current = completeCompaniesDataGlobal;
+  }, [completeCompaniesData, completeCompaniesDataGlobal]);
 
   const syncCompleteDataOnPage = useCallback(
     newData => {
       const rawCompleteData = cloneDeep(
-        selectedType === 'domestic' ? completeCompaniesData : completeCompaniesDataGlobal
+        selectedType === 'domestic' ? completeCompaniesDatalocal.current : completeCompaniesDataGloballocal.current
       );
       if (!rawCompleteData || !isArray(rawCompleteData)) {
         return;
@@ -140,7 +140,7 @@ const Watchlist = props => {
         dispatch(setCompleteGlobalCompaniesData(rawCompleteData));
       }
     },
-    [selectedType, dispatch, completeCompaniesData, completeCompaniesDataGlobal]
+    [selectedType, dispatch]
   );
 
   useEffect(() => {
@@ -320,7 +320,7 @@ const Watchlist = props => {
   const updateChacheData = useCallback(
     (ticker, isTicker) => {
       let rawCompleteData = cloneDeep(
-        selectedType === 'domestic' ? completeCompaniesData : completeCompaniesDataGlobal
+        selectedType === 'domestic' ? completeCompaniesDatalocal : completeCompaniesDataGloballocal.current
       );
       if (Array.isArray(ticker)) {
         for (let i = 0; i < ticker.length; i++) {
@@ -338,7 +338,7 @@ const Watchlist = props => {
         updateTickerValue(rawCompleteData, ticker, isTicker);
       }
     },
-    [dispatch, selectedType, updateTickerValue, completeCompaniesData, completeCompaniesDataGlobal]
+    [dispatch, selectedType, updateTickerValue]
   );
 
   const deleteTicker = useCallback(
