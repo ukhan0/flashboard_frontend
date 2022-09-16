24e5dc1850dbf479e59843a057c48820957d4a49
Muhammad Rehan <73136316+Rehan510@users.noreply.github.com>
2022-09-16 09:22:34 -0400

    resolved doc issue (#446)

--- src/components/watchlist/watchlistApiCalls.js
+++ src/components/watchlist/watchlistApiCalls.js
@@ -11,7 +11,7 @@ export const getWatchlist = (selectedUniverse, selectedFileType, selectedType) =
       dispatch(setCancelExistingDocumentTypeCalls(cancelToken));
       const user = JSON.parse(localStorage.getItem('user'));
       const response = await axios.get(
-        `${config.apiUrl}/api/get_companies_data?auth_token=${user.authentication_token}&user_id=${user.id}&subject=${selectedUniverse}&doc_type=${selectedFileType}&selected_type=${selectedType === 'newGlobal' ? 'global' : selectedType}`,
+        `${config.apiUrl}/api/get_companies_data?auth_token=${user.authentication_token}&user_id=${user.id}&subject=${selectedUniverse}&doc_type=${selectedFileType}&selected_type=${selectedType === 'newGlobal' ? 'domestic' : selectedType}`,
         {
           cancelToken: cancelToken.token
         }
