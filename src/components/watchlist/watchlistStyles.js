b966039a6ab9a93fc6990a2f76ef54fa55e634ca
mueedmalik <mueedalam0@gmil.com>
2022-09-30 20:27:52 +0500

    ui issue resolved.

--- src/components/watchlist/watchlistStyles.js
+++ src/components/watchlist/watchlistStyles.js
@@ -18,7 +18,16 @@ export default makeStyles(() => ({
   },
   button: {
     marginRight: 2,
-    marginLeft: 3
+    marginLeft: 3,
+    width: 'max-content'
+  },
+  addWatchlistBtnContainer: {
+    ['@media (max-width:1057px)']: {
+      paddingTop: '8px'
+    }
+  },
+  allBtnActiveContainer: {
+    paddingTop: '0px'
   },
   loaderContainer: {
     position: 'absolute',
