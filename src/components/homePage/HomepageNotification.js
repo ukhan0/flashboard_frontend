edac4be01f1c84d40993e3be6024a59a15328906
Tariq Sajid <mtariqsajid@gmail.com>
2022-10-12 16:49:30 +0500

    drag color & notification component resiz selction

--- src/components/homePage/HomepageNotification.js
+++ src/components/homePage/HomepageNotification.js
@@ -107,7 +107,7 @@ export default function HomepageNotification() {
         </ButtonGroup>
       </div>
       <div>
-        <div style={{ height: '600px' }}>
+        <div style={{ height: '600px', marginRight: 12 }}>
           <PerfectScrollbar>
             <div className="card-header--title font-weight-bold " style={{ textAlign: 'center', padding: '10px' }}>
               Upcoming Calls
