ec2f32c0a8479405521ad46a868ebadbd218ba8b
Tariq Sajid <mtariqsajid@gmail.com>
2022-10-14 20:32:10 +0500

    disable widget drag/resize when save call is done

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -91,6 +91,7 @@ export default function HomePage() {
     }
 
     setIsHomePageDrawerOpen(false);
+    setEnableDragResizeWidgets(false);
   };
   useEffect(() => {
     dispatch(getUserWatchlist(['domestic', 'global']));
