3758a1fa559576f683a9d5e54b63c0c5f97fab27
Tariq Sajid <mtariqsajid@gmail.com>
2022-10-18 17:04:00 +0500

    add account stock widget onClick set sidebar data

--- src/components/homePage/HomePageAccountStock.js
+++ src/components/homePage/HomePageAccountStock.js
@@ -1,7 +1,38 @@
 import { Card } from "@material-ui/core";
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCompanyByIndex } from '../watchlist/WatchlistHelpers';
+import { setSelectedWatchlist } from '../../reducers/Watchlist';
+import { setIsFromThemex } from '../../reducers/Topic';
+import { setSidebarToggle, setSidebarToggleMobile } from '../../reducers/ThemeOptions';
 
 const HomePageAccountStock = () => {
+    const dispatch = useDispatch();
+    const { completeCompaniesData, completeCompaniesDataGlobal } = useSelector(state => state.Watchlist);
+
+    const setCompany = useCallback(
+        async ticker => {
+            let company = await getCompanyByIndex(ticker, completeCompaniesData, completeCompaniesDataGlobal);
+
+            if (company) {
+                let last10k = new Date(company['last10k']);
+                let last10q = new Date(company['last10q']);
+                company.recentId = last10k > last10q ? company.recentId10k : company.recentId10q;
+            }
+
+            // if recent id not set
+            if (company.recentId === undefined) {
+                company = {};
+            }
+
+            dispatch(setIsFromThemex(false));
+            dispatch(setSelectedWatchlist(company));
+            dispatch(setSidebarToggle(false));
+            dispatch(setSidebarToggleMobile(false));
+        },
+        [dispatch, completeCompaniesData, completeCompaniesDataGlobal]
+    );
+
     useEffect(() => {
         window.SMA.SMAWebAccurateAccount({
             container: 'sma_account_stock_widget',
@@ -11,9 +42,14 @@ const HomePageAccountStock = () => {
             dates: 'datetime+eq+recent',
             frequency: '1',
             sort: 'positive+desc',
-            limit: 10
+            limit: 10,
+            onItemClick: function (ticker) {
+                if (ticker) {
+                    setCompany(ticker);
+                }
+            }
         });
-    }, []);
+    }, [setCompany]);
 
 
     return (
