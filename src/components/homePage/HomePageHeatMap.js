ef418eacb6f4c7c0c7ba0c5169009b8a4f77bd9a
Tariq Sajid <mtariqsajid@gmail.com>
2022-10-26 15:31:51 +0500

    add US and Canada filter in heat-map widget

--- src/components/homePage/HomePageHeatMap.js
+++ src/components/homePage/HomePageHeatMap.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { BeatLoader } from 'react-spinners';
-import { Card } from '@material-ui/core';
+import { Card, ButtonGroup, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+
+const tableTypes = [
+  { name: 'Us', key: 'us', sort: 'ticker', container: 'heatmap-container1' },
+  { name: 'Canada', key: 'canada', sort: 'tsx', container: 'heatmap-container2' }
+];
+
 export default function HomePageHeatMap() {
   const [isLoading, setIsLoading] = useState(true);
+  const [tableType, setTableType] = useState(tableTypes[0]);
   const dispatch = useDispatch();
   useEffect(() => {
     window.SMA.HeatmapWidget({
-      container: 'sma_widget_container',
+      container: tableType['container'],
       width: '100%',
       height: '580',
-      apikey: 'aa0bf3957e823316e5dc18c031c6ddc8074b980d',
-      ontology: 'ticker',
+      apikey: '907cdf5f789e613d36f0b4dda38930fb1dc590bf',
+      ontology: tableType['sort'],
       positiveColor: '00cc00',
       negativeColor: 'cc0000',
       neutralColor: '999999',
@@ -24,7 +31,7 @@ export default function HomePageHeatMap() {
         setIsLoading(false);
       }
     });
-  }, [dispatch]);
+  }, [dispatch, tableType]);
   return (
     <Card className="card-box mb-4" style={{ height: '100%' }}>
       {isLoading ? (
@@ -34,9 +41,25 @@ export default function HomePageHeatMap() {
       ) : null}
       <div className="card-header">
         <div className="card-header--title font-weight-bold drag-handle">Heat Map</div>
+        <ButtonGroup color="primary">
+          {tableTypes.map((type, i) => (
+            <Button
+              size="small"
+              key={`diff_${i}`}
+              onClick={() => setTableType(type)}
+              variant={tableType['key'] === type.key ? 'contained' : 'outlined'}>
+              {type.name}
+            </Button>
+          ))}
+        </ButtonGroup>
       </div>
-      <div style={{ height: '100%', overflow: 'auto' }} id="sma_widget_container">
-      </div>
+
+      {tableType['container'] === 'heatmap-container1' ? (
+        <div style={{ height: '100%', overflow: 'auto' }} id={tableType['container']}></div>
+      ) : null}
+      {tableType['container'] === 'heatmap-container2' ? (
+        <div style={{ height: '100%', overflow: 'auto' }} id={tableType['container']}></div>
+      ) : null}
     </Card>
   );
 }
