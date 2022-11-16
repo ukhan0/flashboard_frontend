4c0142b16e49662433957fc1b0ec14ad969efb42
mueedmalik <mueedalam0@gmil.com>
2022-11-16 21:04:41 +0500

    Removed warning: Failed prop type

--- src/components/topic/TopicSearchDropDown.js
+++ src/components/topic/TopicSearchDropDown.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import useStyles from '../watchlist/watchlistStyles';
 import CloseIcon from '@material-ui/icons/Close';
-const TopicSearchDropDown = props => {
+const TopicSearchDropDown = ({ onClose, ...props }) => {
+
+  useEffect(() => {
+    const clearBtn = document.getElementById("country_autocomplete_close_btn").parentElement.parentElement;
+    const handleClick = () => {
+      onClose();
+    }
+    if (clearBtn) {
+      clearBtn.addEventListener("click", handleClick);
+    };
+    return () => {
+      clearBtn.removeEventListener('click', handleClick);
+    };
+  }, [onClose])
+
   const classes = useStyles();
   return (
     <FormControl className={classes.formControl}>
@@ -19,9 +33,7 @@ const TopicSearchDropDown = props => {
         closeIcon={
           props.closeIcon ? (
             <CloseIcon
-              onClick={() => {
-                props.onClose();
-              }}
+              id='country_autocomplete_close_btn'   //do not change or delete ID, an event is being listned through this ID
               fontSize="small"
             />
           ) : null
